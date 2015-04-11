3b56aa0647dbf38dbd76016e170b44a4b595aac9
briankeane <lonesomewhistle@gmail.com>
2015-04-11 12:05:56 -0500

    fixed ffmpeg Vagrant install

--- vagrant/rootScript.sh
+++ vagrant/rootScript.sh
@@ -9,10 +9,13 @@ sudo apt-get update
 sudo apt-get install -y libtag1-dev
 sudo apt-get install -y libav-tools
 sudo apt-get install -y libmp3lame-dev
+
+sudo add-apt-repository ppa:mc3man/trusty-media
 sudo apt-get update
-sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev
+sudo install ffmpeg
 mkdir ~/ffmpeg_sources
 
+# mongo
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
