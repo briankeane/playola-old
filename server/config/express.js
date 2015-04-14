8d9d242dbd9daa17f464adfe801d566997a3b39c
briankeane <lonesomewhistle@gmail.com>
2015-04-14 15:29:38 -0500

    new version

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,6 @@
 'use strict';
 
 var express = require('express');
-var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -42,7 +41,6 @@ module.exports = function(app) {
   }));
   
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
