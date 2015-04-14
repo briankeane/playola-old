3662b6da7ca37a6f3cc9726ba7ad4daaf8ab11be
briankeane <lonesomewhistle@gmail.com>
2015-04-14 14:26:18 -0500

    New version

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,14 +5,13 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://172.31.51.139:27017/playola'
-    //uri: 'mongodb://localhost/pl2nodeyo-dev'
+    uri: 'mongodb://localhost/pl2nodeyo-dev'
   },
 
   seedDB: false,
 
   s3Buckets: { 
-    SONGS_BUCKET: 'playolasongs',
+    SONGS_BUCKET: 'playolasongsdevelopment',
     COMMERCIALS_BUCKET: 'playolacommercialblocks',
     COMMENTARIES_BUCKET: 'playolacommentariesdevelopment',
     UNPROCESSED_SONGS_BUCKET: 'playolaunprocessedsongsdevelopment'
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,7 +18,7 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/www'
+            'mongodb://172.31.51.139:27017/playola'
   },
 
   s3Buckets: { 
