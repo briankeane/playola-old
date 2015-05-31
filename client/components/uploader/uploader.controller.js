d7adac3cb94a75b733710a368b76740e69160598
briankeane <lonesomewhistle@gmail.com>
2015-05-31 12:10:57 -0500

    Added uploader.service

--- client/components/uploader/uploader.controller.js
+++ client/components/uploader/uploader.controller.js
@@ -1,220 +1,162 @@
 angular.module('playolaApp')
-  .controller('uploaderCtrl', function ($scope, Auth, FileUploader, $modal) {
-    $scope.uploader = new FileUploader({ url: 'http://upload.playola.fm/uploads',
-                                        // headers: { 'Access-Control-Allow-Origin': '*' },
-                                          //withCredentials: true,
-                                          autoUpload: true });
+  .controller('uploaderCtrl', function ($scope, Auth, Uploader, $modal) {
+    $scope.uploader = Uploader.fileUploader
 
-      // $scope.uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-      //     console.info('onWhenAddingFileFailed', item, filter, options);
-      // };
-      // $scope.uploader.onAfterAddingFile = function(fileItem) {
-      //     console.info('onAfterAddingFile', fileItem);
-      // };
-      // $scope.uploader.onAfterAddingAll = function(addedFileItems) {
-      //     console.info('onAfterAddingAll', addedFileItems);
-      // };
-      $scope.uploader.onBeforeUploadItem = function(item) {
-          // set status to 'Uploading'
-          item.status = 'Uploading';
-      };
-      $scope.uploader.onProgressItem = function(fileItem, progress) {
-          // switch status to processing after upload is complete
-          if (progress === 100) {
-            fileItem.status = 'Processing';
+    $scope.addToMyStation = function (songId) {
+      Auth.createRotationItem({ weight: 17,
+                                bin: 'active',
+                                _song: songId }, function (err, results) {
+        if (!err){
+          for (var i=0;i<$scope.uploader.queue.length;i++) {
+            if ($scope.uploader.queue[i].songId === songId) {
+              $scope.uploader.queue[i].addedToStation = true;
+
+            }
           }
-      };
-      // $scope.uploader.onProgressAll = function(progress) {
-      //     console.info('onProgressAll', progress);
-      // };
-      // $scope.uploader.onSuccessItem = function(fileItem, response, status, headers) {
-      //     console.info('onSuccessItem', fileItem, response, status, headers);
-      // };
-      // $scope.uploader.onErrorItem = function(fileItem, response, status, headers) {
-      //     console.info('onErrorItem', fileItem, response, status, headers);
-      // };
-      // $scope.uploader.onCancelItem = function(fileItem, response, status, headers) {
-      //     console.info('onCancelItem', fileItem, response, status, headers);
-      // };
-
-      $scope.uploader.onCompleteItem = function(fileItem, response, status, headers) {
-        if (response.status === 'More Info Needed') {
-          fileItem.status = response.status;
-          fileItem.uploadItem = response.upload
-          fileItem.uploadId = response.upload._id;
-        } else if (response.status === 'Song Already Exists') {
-          fileItem.status = 'Success';
-          fileItem.songId = response.song._id;
-        } else if (response.status === 'added') {
-          fileItem.status = 'Success';
-          fileItem.songId = response.song._id;
         }
+      });
+    };
 
-      };
-      $scope.uploader.onCompleteAll = function() {
-          console.info('onCompleteAll');
-      };
+    $scope.getSongMatch = function (item) {
+      $modal.open({
+        templateUrl: 'components/uploader/getMatch.modal.html',
+        size: 'lg',
+        scope: $scope,  
+        controller: function ($modalInstance) {
 
-      $scope.addToMyStation = function (songId) {
-        Auth.createRotationItem({ weight: 17,
-                                  bin: 'active',
-                                  _song: songId }, function (err, results) {
-          if (!err){
-            for (var i=0;i<$scope.uploader.queue.length;i++) {
-              if ($scope.uploader.queue[i].songId === songId) {
-                $scope.uploader.queue[i].addedToStation = true;
+          $scope.item = item;
+          $scope.selectedSong = {}
+          $scope.selectedSong.index = '';
+          $scope.oldTitle = item.uploadItem.tags.title;
+          $scope.oldAlbum = item.uploadItem.tags.album;
+          $scope.oldArtist = item.uploadItem.tags.artist;
+          $scope.tagsChanged = false;
+          $scope.tags = item.uploadItem.tags;
 
-              }
-            }
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+
+          $scope.setValue = function () {
+            $scope.selectedSong.index = 'ECHONESTIDNOTFOUND';
           }
-        });
-      };
 
-      $scope.getSongMatch = function (item) {
-        $modal.open({
-          templateUrl: 'components/uploader/getMatch.modal.html',
-          size: 'lg',
-          scope: $scope,  
-          controller: function ($modalInstance) {
+          $scope.submitForm = function () {
+            $scope.submitted = true;
+            
+            // IF it was not found, resubmit request...
+            if ($scope.selectedSong.index === 'ECHONESTIDNOTFOUND') {
+              $modalInstance.dismiss('close');
+              // see if new tags are needed
+              $modal.open({
+                templateUrl: 'components/uploader/getTags.modal.html',
+                size: 'med',
+                scope: $scope,
+                controller: function ($modalInstance) {
+                  $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                  };
 
-            $scope.item = item;
-            $scope.selectedSong = {}
-            $scope.selectedSong.index = '';
-            $scope.oldTitle = item.uploadItem.tags.title;
-            $scope.oldAlbum = item.uploadItem.tags.album;
-            $scope.oldArtist = item.uploadItem.tags.artist;
-            $scope.tagsChanged = false;
-            $scope.tags = item.uploadItem.tags;
+                  $scope.submitTagForm = function () {
+                    if ($scope.tagsChanged) {
+                      var uploadInfo = { uploadId: item.uploadItem._id,
+                                          tags: $scope.tags }
+                      Auth.submitUploadWithUpdatedTags(uploadInfo, function (err, response) {
+                        // IF the song was found
+                        if (response.status === 'Song Added' || 'Song Already Exists') {
+                          item.isNeedInfo = false;
+                          item.status = response.status;
+                          item.songId = response.song._id;
+                          $modalInstance.dismiss('close');
+                        
+                        // ELSE (no  was found)
+                        } else {
 
-            $scope.cancel = function () {
-              $modalInstance.dismiss('cancel');
-            };
-
-            $scope.setValue = function () {
-              $scope.selectedSong.index = 'ECHONESTIDNOTFOUND';
-            }
-
-            $scope.submitForm = function () {
-              $scope.submitted = true;
-              
-              // IF it was not found, resubmit request...
-              if ($scope.selectedSong.index === 'ECHONESTIDNOTFOUND') {
-                $modalInstance.dismiss('close');
-                // see if new tags are needed
-                $modal.open({
-                  templateUrl: 'components/uploader/getTags.modal.html',
-                  size: 'med',
-                  scope: $scope,
-                  controller: function ($modalInstance) {
-                    $scope.cancel = function () {
-                      $modalInstance.dismiss('cancel');
-                    };
-
-                    $scope.submitTagForm = function () {
-                      if ($scope.tagsChanged) {
-                        var uploadInfo = { uploadId: item.uploadItem._id,
-                                            tags: $scope.tags }
-                        Auth.submitUploadWithUpdatedTags(uploadInfo, function (err, response) {
-                          // IF the song was found
-                          if (response.status === 'Song Added' || 'Song Already Exists') {
-                            item.isNeedInfo = false;
-                            item.status = response.status;
-                            item.songId = response.song._id;
-                            $modalInstance.dismiss('close');
-                          
-                          // ELSE (no  was found)
-                          } else {
-
-                          }
-                        })
-                      
-                      // ELSE tags have not changed
-                      } else {
-                        // add without echonestId
-                        Auth.submitUploadWithoutEchonestId
-                      }
-                    }
-
-                  }
-                })
-
-
-
-
-
-                if ($scope.tagsChanged) {
-                  var uploadInfo = { uploadId: item.upload._id,
-                                     tags: item.tags }
-                  Auth.submitUploadWithoutEchonestId(uploadInfo, function (err, response) {
-                    if (err) {
-
+                        }
+                      })
+                    
+                    // ELSE tags have not changed
                     } else {
-                      if (response.status === 'Song Added') {
-                        item.isNeedInfo = false;
-                        item.status = response.status
-                        item.isSuccess = true;
-                        item.songId = response.song._id;
-                        $modalInstance.dismiss('close');
-                      }
+                      // add without echonestId
+                      Auth.submitUploadWithoutEchonestId
                     }
-                  });
+                  }
+
                 }
-              // if echonestID was provided, resubmit upload
-              }  else {
-                var index = parseInt($scope.selectedSong.index);
-                var uploadInfo = { artist: item.uploadItem.possibleMatches[index].artist,
-                                    title: item.uploadItem.possibleMatches[index].title,
-                                    echonestId: item.uploadItem.possibleMatches[index].echonestId,
-                                    album: item.uploadItem.possibleMatches[index].album || item.uploadItem.album,
-                                    uploadId: item.uploadItem._id,
-                                    tags: item.uploadItem.tags
-                                  };
-                Auth.submitUploadViaEchonestId(uploadInfo, function (err, response) {
-                  if (response.status === 'Song Already Exists' || 'Song Added') {
-                    item.isNeedInfo = false;
-                    item.status = response.status;
-                    item.isSuccess = true;
-                    item.songId = response.song._id;
-                    $modalInstance.dismiss('close');
+              })
+
+              if ($scope.tagsChanged) {
+                var uploadInfo = { uploadId: item.upload._id,
+                                   tags: item.tags }
+                Auth.submitUploadWithoutEchonestId(uploadInfo, function (err, response) {
+                  if (err) {
+
+                  } else {
+                    if (response.status === 'Song Added') {
+                      item.isNeedInfo = false;
+                      item.status = response.status
+                      item.isSuccess = true;
+                      item.songId = response.song._id;
+                      $modalInstance.dismiss('close');
+                    }
                   }
                 });
-
               }
-            };
-
-          }
-        }).result.then(function () {
-          if ($scope.selectedSong.index === 'ECHONESTIDNOTFOUND') {
-            if ($scope.tagsChanged) {
-              var uploadInfo = { uploadId: item.uploadId,
-                                 tags: item.tags
+            // if echonestID was provided, resubmit upload
+            }  else {
+              var index = parseInt($scope.selectedSong.index);
+              var uploadInfo = { artist: item.uploadItem.possibleMatches[index].artist,
+                                  title: item.uploadItem.possibleMatches[index].title,
+                                  echonestId: item.uploadItem.possibleMatches[index].echonestId,
+                                  album: item.uploadItem.possibleMatches[index].album || item.uploadItem.album,
+                                  uploadId: item.uploadItem._id,
+                                  tags: item.uploadItem.tags
                                 };
-              
-              Auth.resubmitUploadWithUpdatedTags(uploadInfo, function (err, response) {
-                if (response.status === 'info needed') {
-                  item.status = response.status;
-                  item.possibleMatches = response.possibleMatches;
-                  item.tags = response.tags;
-                  item.filename = response.filename;
-                  item.isSuccess = false;
-                  item.isNeedInfo = true;
-                  item.uploadId = response._id;
-                } else if (response.status === 'Song Already Exists') {
+              Auth.submitUploadViaEchonestId(uploadInfo, function (err, response) {
+                if (response.status === 'Song Already Exists' || 'Song Added') {
                   item.isNeedInfo = false;
                   item.status = response.status;
                   item.isSuccess = true;
                   item.songId = response.song._id;
-
-                } else if (response.status === 'added') {
-                  item.isNeedInfo = false;
-                  item.status = response.status;
-                  item.isSuccess = true;
-                  item.songId = response.song._id;
+                  $modalInstance.dismiss('close');
                 }
               });
-            }
-          }
 
-        });
-      };
+            }
+          };
+
+        }
+      }).result.then(function () {
+        if ($scope.selectedSong.index === 'ECHONESTIDNOTFOUND') {
+          if ($scope.tagsChanged) {
+            var uploadInfo = { uploadId: item.uploadId,
+                               tags: item.tags
+                              };
+            
+            Auth.resubmitUploadWithUpdatedTags(uploadInfo, function (err, response) {
+              if (response.status === 'info needed') {
+                item.status = response.status;
+                item.possibleMatches = response.possibleMatches;
+                item.tags = response.tags;
+                item.filename = response.filename;
+                item.isSuccess = false;
+                item.isNeedInfo = true;
+                item.uploadId = response._id;
+              } else if (response.status === 'Song Already Exists') {
+                item.isNeedInfo = false;
+                item.status = response.status;
+                item.isSuccess = true;
+                item.songId = response.song._id;
+
+              } else if (response.status === 'added') {
+                item.isNeedInfo = false;
+                item.status = response.status;
+                item.isSuccess = true;
+                item.songId = response.song._id;
+              }
+            });
+          }
+        }
+      });
+    };
   });
---
+++ client/components/uploader/uploader.service.js
@@ -0,0 +1,65 @@
+'use strict';
+
+angular.module('playolaApp')
+  .service('Uploader', function ($rootScope, FileUploader, Auth, $q) {
+
+    var self =this;
+
+    this.fileUploader = new FileUploader({ url: 'http://upload.playola.fm/uploads',
+                                        // headers: { 'Access-Control-Allow-Origin': '*' },
+                                          //withCredentials: true,
+                                          autoUpload: true });
+
+    // this.fileUploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+    //     console.info('onWhenAddingFileFailed', item, filter, options);
+    // };
+    // this.fileUploader.onAfterAddingFile = function(fileItem) {
+    //     console.info('onAfterAddingFile', fileItem);
+    // };
+    // this.fileUploader.onAfterAddingAll = function(addedFileItems) {
+    //     console.info('onAfterAddingAll', addedFileItems);
+    // };
+    this.fileUploader.onBeforeUploadItem = function(item) {
+        // set status to 'Uploading'
+        item.status = 'Uploading';
+    };
+    this.fileUploader.onProgressItem = function(fileItem, progress) {
+        // switch status to processing after upload is complete
+        if (progress === 100) {
+          fileItem.status = 'Processing';
+        }
+    };
+    // this.fileUploader.onProgressAll = function(progress) {
+    //     console.info('onProgressAll', progress);
+    // };
+    // this.fileUploader.onSuccessItem = function(fileItem, response, status, headers) {
+    //     console.info('onSuccessItem', fileItem, response, status, headers);
+    // };
+    // this.fileUploader.onErrorItem = function(fileItem, response, status, headers) {
+    //     console.info('onErrorItem', fileItem, response, status, headers);
+    // };
+    // this.fileUploader.onCancelItem = function(fileItem, response, status, headers) {
+    //     console.info('onCancelItem', fileItem, response, status, headers);
+    // };
+
+    this.fileUploader.onCompleteItem = function(fileItem, response, status, headers) {
+      if (response.status === 'More Info Needed') {
+        fileItem.status = response.status;
+        fileItem.uploadItem = response.upload
+        fileItem.uploadId = response.upload._id;
+      } else if (response.status === 'Song Already Exists') {
+        fileItem.status = 'Success';
+        fileItem.songId = response.song._id;
+      } else if (response.status === 'added') {
+        fileItem.status = 'Success';
+        fileItem.songId = response.song._id;
+      }
+
+    };
+    this.fileUploader.onCompleteAll = function() {
+        console.info('onCompleteAll');
+    };
+
+
+
+  })
